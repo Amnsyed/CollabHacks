d651a49ce9501dbfaabbabb6a800c8547ff57c10
Glitch (amanuddin2002-collabhacks) <none>
2019-07-16 04:47:39 +0000

    🚪👣 Checkpoint ./src/constants.js:5498798/938 ./src/routers/rooms.js:5498798/82

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+/*const dotenv = require('dotenv');
 const axios = require('axios');
 
 
@@ -27,4 +27,19 @@ module.exports = {
         headers: {'Accept': 'text/plain'}
     }),
     botName: 'Dad'
+};*/
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+const My_TOKEN = 'Y2lzY29zcGFyazovL3VzL0FQUExJQ0FUSU9OL2EyNmU2YzdiLTU4OGYtNDYzNS1iMGUwLWU5NzdiNTFkMDJmZA'
+const BOT_TOKEN = 'NjMyM2IzMGItMmFhOC00MmZhLWIzYjgtMDMzYzgwZGFjYmM2MzFkOGRlMmEtOTQ1_PF84_consumer'
+const BOT = false;
+
+module.exports ={
+    PORT : 3000,
+    webexAxiosInstance : axios.create({
+      baseURL: 'https://api.ciscospark.com/v1',
+      timeout: 1000,
+      headers: {'authorization':  'Bearer ${BOT ? BOT_TOKEN : MY_TOKEN}'}
+})
 };
--- src/routers/rooms.js
+++ src/routers/rooms.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const constants = require('../constants');
 
 router.get('/:roomId/messages', function(request, response) {
-    const roomId = request.params['roomId'];
+    const roomId = request.params['Y2lzY29zcGFyazovL3VzL1JPT00vOWEzZDA5MTAtYTI3Ny0xMWU5LTlhZDEtNjEwNzhlMDZjYmI0'];
 
     return constants
     .webexAxiosInstance
